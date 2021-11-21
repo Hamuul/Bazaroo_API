e631c2a2692a8fdd54ac7bbc4a16e918ac010ab9
Hamuul <hamuul@disroot.org>
2021-11-21 18:33:59 +0200

    added User model

--- APP/models.py
+++ APP/models.py
@@ -12,3 +12,14 @@ class ContactRequest(Base):
     name = Column(String(length=50))
     email = Column(String(length=50), index=True)
     is_resolved = Column(Boolean, default=False, nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String(length=50))
+    lastname = Column(String(length=50))
+    email = Column(String(length=50), unique=True, index=True)
+    password = Column(String(length=256))
+    role = Column(String(length=7))
