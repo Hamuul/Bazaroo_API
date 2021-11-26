2ec20a3c7cef89ec04b26ee1a0bed91f0093b2d3
Hamuul <hamuul@disroot.org>
2021-11-26 17:17:49 +0200

    added schemas needed for new endpoints

--- APP/schemas.py
+++ APP/schemas.py
@@ -1,84 +1,146 @@
 from pydantic import (BaseModel,
                       EmailStr,
                       StrictBool,
-                      StrictStr,
                       StrictInt,
-                      validator)
+                      validator,
+                      constr)
 
 from typing import Optional
+from fastapi import HTTPException, status
 
 
 class ContactRequestIn(BaseModel):
-    message: StrictStr
-    name: StrictStr
+    message: constr(strict=True, max_length=5000)
+    name: constr(strict=True, max_length=50)
     email: EmailStr
-
-    @validator('name')
-    def name_must_be_under_50_chars(cls, v):
-        assert(len(v) < 50), "must have under 50 chars"
-        return v
-
-    @validator('email')
-    def email_must_be_under_50_chars(cls, v):
-        assert(len(v) < 50), "must have under 50 chars"
-        return v
-
-    @validator('message')
-    def message_must_be_under_50_chars(cls, v):
-        assert(len(v) < 50), "must have under 50 chars"
-        return v
 
     class Config:
         orm_mode = True
 
+    @validator('email')
+    def email_under_50_chars(cls, v):
+        if len(v) > 50:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='email must be no longer than 50 chars')
+        return v
+
 
 class ContactRequestOut(BaseModel):
     id: Optional[StrictInt]
-    message: Optional[StrictStr]
-    name: Optional[StrictStr]
+    message: Optional[constr(strict=True, max_length=5000)]
+    name: Optional[constr(strict=True, max_length=50)]
     email: Optional[EmailStr]
     is_resolved: StrictBool
 
     class Config:
         orm_mode = True
 
+    @validator('email')
+    def email_under_50_chars(cls, v):
+        if len(v) > 50:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='email must be no longer than 50 chars')
+        return v
+
 
 class UserShow(BaseModel):
     id: Optional[StrictInt]
     email: Optional[EmailStr]
-    firstname: Optional[StrictStr]
-    lastname: Optional[StrictStr]
-    role: Optional[StrictStr]
+    firstname:  constr(strict=True, max_length=50)
+    lastname: constr(strict=True, max_length=50)
+    role: constr(strict=True, max_length=7)
 
-    @validator('lastname')
-    def lastname_must_be_under_50_chars(cls, v):
-        assert(len(v) < 50), 'must have under 50 chars'
+    class Config:
+        orm_mode = True
+
+    @validator('role')
+    def member(cls, v):
+        if (v not in ['student', 'teacher']):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='must be student or teacher')
         return v
 
-    @validator('firstname')
-    def firstname_must_be_under_50_chars(cls, v):
-        assert(len(v) < 50), 'must have under 50 chars'
+    @validator('email')
+    def email_under_50_chars(cls, v):
+        if len(v) > 50:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='email must be no longer than 50 chars')
         return v
+
+
+class UserSignUp(UserShow):
+    password: constr(strict=True, min_length=8, max_length=50)
+    confirmation_password: constr(strict=True, min_length=8, max_length=50)
+
+
+class UserUpdate(BaseModel):
+    id: Optional[StrictInt]
+    email: Optional[EmailStr]
+    firstname: Optional[constr(strict=True, max_length=50)]
+    lastname: Optional[constr(strict=True, max_length=50)]
+    role: Optional[constr(strict=True, max_length=7)]
+    password: Optional[constr(strict=True, min_length=8, max_length=50)]
+    confirmation_password: Optional[constr(strict=True, min_length=8,
+                                           max_length=50)]
+
+    class Config:
+        orm_mode = True
 
     @validator('role')
     def member(cls, v):
         assert(v in ['student', 'teacher']), 'must be student or teacher'
         return v
 
+    @validator('email')
+    def email_under_50_chars(cls, v):
+        if len(v) > 50:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='email must be no longer than 50 chars')
+        return v
+
+
+class LoginForm(BaseModel):
+    email: EmailStr
+    password: constr(strict=True, min_length=8, max_length=50)
+
     class Config:
         orm_mode = True
 
 
-class UserSignUp(UserShow):
-    password: StrictStr
-    confirmation_password: StrictStr
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-    @validator('password')
-    def pass_between_8_and_50_chars(cls, v):
-        assert(len(v) < 50 and len(v) > 8), 'must have between 8 and 50 chars'
-        return v
 
-    @validator('confirmation_password')
-    def confpass_between_8_and_50_chars(cls, v):
-        assert(len(v) < 50 and len(v) > 8), 'must have between 8 and 50 chars'
-        return v
+class TokenData(BaseModel):
+    email: Optional[EmailStr] = None
+
+
+class Review(BaseModel):
+    id: Optional[StrictInt]
+    message: constr(strict=True, max_length=500)
+    user_id: Optional[StrictInt]
+
+    class Config:
+        orm_mode = True
+
+
+class ClassShow(BaseModel):
+    id: StrictInt
+    description: constr(strict=True, max_length=500)
+    teacher_id: StrictInt
+    subject: constr(strict=True, max_length=80)
+
+    class Config:
+        orm_mode = True
+
+
+class ClassUpdate(BaseModel):
+    description: constr(strict=True, max_length=500)
+
+    class Config:
+        orm_mode = True
+
+
+class ClassCreate(ClassUpdate):
+    subject: constr(strict=True, max_length=80)
