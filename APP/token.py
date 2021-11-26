977eb7de9132360b576195951d9b2d6c91715d41
Hamuul <hamuul@disroot.org>
2021-11-26 17:18:24 +0200

    added jwt authentication for login endpoint

---
+++ APP/token.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+
+from . import schemas
+
+SECRET_KEY = '7a3566151b9a7694e282a14645b2f610f400d5efc8c0b0b6f58e91c427ecd4b3'
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
