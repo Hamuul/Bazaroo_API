4e02c31e406ab641eb1a12503d9ee4e8c42dd22e
Hamuul <hamuul@disroot.org>
2021-11-26 17:15:47 +0200

    added authentication, password hashing

--- APP/routers/auth.py
+++ APP/routers/auth.py
@@ -2,14 +2,15 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from ..database import get_db
-from ..schemas import UserSignUp, UserShow
+from ..schemas import UserSignUp, LoginForm
 from ..hashing import Hash
+from ..token import create_access_token
 from .. import models
 
 router = APIRouter()
 
 
-@router.post("/auth/register", status_code=status.HTTP_201_CREATED)
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 def create_user(req: UserSignUp, db: Session = Depends(get_db)):
     valid_email = bool((
         req.role == 'student' and req.email.endswith("@stud.upb.ro"))
@@ -26,6 +27,13 @@ def create_user(req: UserSignUp, db: Session = Depends(get_db)):
     return 'User was created successfully'
 
 
-@router.post("/auth/login", response_model=UserShow)
-def login(db: Session = Depends(get_db)):
-    pass
+@router.post("/login")
+def login(login_req: LoginForm, db: Session = Depends(get_db)):
+    query = db.query(models.User).\
+        filter(models.User.email == login_req.email).first()
+    if query is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if not Hash.verify_password(login_req.password, query.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token(data={"sub": login_req.email})
+    return {"access_token": access_token, "token_type": "bearer"}
